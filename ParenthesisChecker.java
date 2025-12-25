1d3d9330566a127fc5687ce02b3f168d773b34b5
AkhilPalli7866 <akhilpalli786@gmail.com>
2025-12-25 15:57:18 +0000

    final comit completed

--- ParenthesisChecker.java
+++ ParenthesisChecker.java
@@ -1,58 +1,49 @@
 //  Class author: Akhil Palli
 //  Date created:  11/12/2025
-//  General description: This program checks if the parentheses are properly written 
-//                       and match up
-
-
-
-// Hey Mr Menchukov I just needed some more time to work on this assignment, I'm not able to figure it out. 
-
-
-
+//  General description: This program checks if the parentheses are properly written and match up
 public class ParenthesisChecker {
 
-    
-    public static void checkParentheses(String s) {
-        int openCount = 0;
-        int i = 0;
 
+    //precondition: method accepts stirng values of different sets of brackets.
+    //postcondition: method checks validity of the stirng input of the different types of parentheses brackets and curly braces.
+    public static boolean isValid(String s1){
+        boolean valid  = false;
+        String text = s1; 
+        String expectations = "";
        
-        //  Pre-condition: The string uses '(' and ')' only ;-;.
-        //  Post-condition: Prints if the parentheses look valid or not.
-        while (i < s.length()) {
-            char ch = s.charAt(i) ;
+            for (int i = 0; i<= text.length()-1 ;i++){
+                String ch = text.substring(i,i+1);
+                    if(ch.equals("(")){ expectations += ")";}
+                    else if(ch.equals("{")){ expectations += "}"; }
+                    else if (ch.equals("[")){ expectations += "]"; }
 
-            if (  ch   ==   '('  ) {
-                openCount++;
+                    else if (ch.equals(")")||ch.equals("}" ) || ch.equals("]")){
+                        if (expectations.length() == 0 ){
+                            return false; 
+                        }
+                        String lastChExpec = expectations.substring(expectations.length()-1);
 
-            } 
-            else if (  ch == ')' ) {
-                openCount-= 1;
-
+                        if(!(ch.equals(lastChExpec))){
+                            return false;
+                        }
+                        expectations = expectations.substring(0,expectations.length()-1);
+                    }
+                    else { return false;} 
             }
-
-            i+=1 ;
-          }
-
-        if (openCount == 0){
-            System.out.println("Valid");
-        } 
-        else{
-            System.out.println("Invalid");
-        }
+         if (expectations.length()== 0){return true;}
+         else {return false;}
     }
-
 
 
     //pre conditon : main method to check
     //post condition : runs through parentheses checker method to chevck if parentheses are valid or not.
     public static void main(String[] args) {
-        
-
-        String test = "(()))("; 
-
-        
-        checkParentheses(test);
+        System.out.println(isValid("(){}[]")); //true
+        System.out.println(isValid("(]")); //false
+        System.out.println(isValid("([{({})}])")); //true
+        System.out.println(isValid("(([]){})")); //true
+        System.out.println(isValid("{}[())()(]")); //false
+        System.out.println(isValid("(){}{")); //false
     }
 }
 
